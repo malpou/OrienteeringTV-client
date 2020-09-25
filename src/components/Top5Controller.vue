9295cd8217fb19fd1cde875bc1e17cd5652f9777
Malthe Poulsen <malthe@grundtvigsvej.dk>
2020-09-25 21:34:16 +0200

    ➕ Making top5 panel to a component

---
+++ src/components/Top5Controller.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <h1>Results (Top 5)</h1>
+    <v-col cols="12">
+      <v-row align="center" justify="center">
+        <v-card
+          v-for="(radio, index) in pickedClass.radios"
+          :key="radio"
+          class="ma-3 pa-6"
+          outlined
+          tile
+        >
+          Control: {{ radio }}
+          <br />
+          <v-btn
+            @click="btnTop5(radio, index)"
+            color="red"
+            class="white--text"
+            :disabled="serviceRunning && runningIndex !== index"
+            :class="{ green: runningIndex === index }"
+          >
+            {{ runningIndex === index ? "Stop Service" : "Start Service" }}
+          </v-btn>
+        </v-card>
+      </v-row>
+    </v-col>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import store from "@/store";
+import { checkForChanges } from "@/utils/checkForChanges";
+import { updateTop5 } from "@/api/top5";
+
+export default Vue.extend({
+  name: "Top5Controller",
+  data() {
+    return {
+      runningIndex: null as number | null,
+      serviceRunning: null as boolean | null
+    };
+  },
+  methods: {
+    btnTop5(radio: string, index: number) {
+      if (!this.serviceRunning) {
+        this.runningIndex = index;
+        this.serviceRunning = true;
+        this.serviceTop5(radio);
+      } else {
+        this.runningIndex = null;
+        this.serviceRunning = false;
+      }
+    },
+    async serviceTop5(radio: string): Promise<void> {
+      while (this.serviceRunning) {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        if (await checkForChanges(this.pickedClass)) {
+          updateTop5(this.pickedClass.id, radio);
+        }
+      }
+    }
+  },
+  computed: {
+    pickedClass() {
+      return store.state.pickedClass;
+    }
+  }
+});
+</script>
--- src/views/Controller.vue
+++ src/views/Controller.vue
@@ -1,81 +1,20 @@
 <template>
   <v-container fluid>
-    <h1 v-if="isClassPicked">Results (Top 5)</h1>
-    <v-col cols="12">
-      <v-row align="center" justify="center">
-        <v-card
-          v-for="(radio, index) in pickedClass.radios"
-          :key="radio"
-          class="ma-3 pa-6"
-          outlined
-          tile
-        >
-          Control: {{ radio }}
-          <br />
-          <v-btn
-            @click="btnTop5(radio, index)"
-            color="red"
-            class="white--text"
-            :disabled="serviceRunning && runningIndex !== index"
-            :class="{ green: runningIndex === index }"
-          >
-            {{ runningIndex === index ? "Stop Service" : "Start Service" }}
-          </v-btn>
-        </v-card>
-      </v-row>
-    </v-col>
+    <top-5-controller v-if="isClassPicked" />
   </v-container>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import store from "@/store";
-import { checkForChanges } from "@/utils/checkForChanges";
-import { updateTop5 } from "@/api/top5";
+import Top5Controller from "@/components/Top5Controller.vue";
 
 export default Vue.extend({
   name: "Controller",
-  data() {
-    return {
-      runningIndex: null as number | null,
-      serviceRunning: null as boolean | null
-    };
-  },
-  methods: {
-    btnTop5(radio: string, index: number) {
-      if (!this.serviceRunning) {
-        this.runningIndex = index;
-        this.serviceRunning = true;
-        this.serviceTop5(radio);
-      } else {
-        this.runningIndex = null;
-        this.serviceRunning = false;
-      }
-    },
-    async serviceTop5(radio: string): Promise<void> {
-      while (this.serviceRunning) {
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        if (await checkForChanges(store.state.pickedClass)) {
-          updateTop5(store.state.pickedClass.id, radio);
-        }
-      }
-    }
-  },
+  components: { Top5Controller },
   computed: {
-    connection() {
-      return store.state.connectionStatus;
-    },
     isClassPicked() {
       return store.state.isClassPicked;
-    },
-    pickedClass() {
-      return store.state.pickedClass;
-    },
-    isRunnerPicked() {
-      return store.state.isRunnerPicked;
-    },
-    pickedRunner() {
-      return store.state.pickedRunner;
     }
   }
 });
