eb8ed71af21b9deec2099e141734027a787adc7d
Malthe Poulsen <malthep@wizdom-intranet.com>
2020-10-14 11:21:17 +0200

    Adding Startlist Controller

---
+++ src/components/StartlistController.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>Startlist</h1>
+    <v-col cols="12">
+      <v-row align="center" justify="center">
+        <v-card class="ma-3 pa-6" outlined tile>
+          Update Startlist
+          <br />
+          <v-btn
+            @click="btnStartlist"
+            color="red"
+            class="white--text"
+            :disabled="loading"
+          >
+            Update
+          </v-btn>
+        </v-card>
+      </v-row>
+    </v-col>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import store from "@/store";
+
+export default Vue.extend({
+  name: "StartlistController",
+  data() {
+    return {
+      loading: false
+    };
+  },
+  methods: {
+    btnStartlist() {
+      this.loading = true;
+      this.serviceStartlist();
+      this.loading = false;
+    },
+    serviceStartlist() {
+      console.log("Startlist Updated");
+    }
+  },
+  computed: {
+    pickedClass() {
+      return store.state.pickedClass;
+    }
+  }
+});
+</script>
--- src/views/Controller.vue
+++ src/views/Controller.vue
@@ -1,6 +1,7 @@
 <template>
   <v-container fluid>
     <top-5-controller v-if="isClassPicked" />
+    <startlist-controller v-if="isClassPicked" />
   </v-container>
 </template>
 
@@ -8,10 +9,11 @@
 import Vue from "vue";
 import store from "@/store";
 import Top5Controller from "@/components/Top5Controller.vue";
+import StartlistController from "@/components/StartlistController.vue";
 
 export default Vue.extend({
   name: "Controller",
-  components: { Top5Controller },
+  components: { Top5Controller, StartlistController },
   computed: {
     isClassPicked() {
       return store.state.isClassPicked;
