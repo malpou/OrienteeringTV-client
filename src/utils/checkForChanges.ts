bc7c396bf3c4a0fefd8eac93c3f3475c78630b46
Malthe Poulsen <malthe@grundtvigsvej.dk>
2020-10-14 20:11:36 +0200

    Fixing Check for chancges

--- src/utils/checkForChanges.ts
+++ src/utils/checkForChanges.ts
@@ -1,14 +1,12 @@
 import store from "@/store";
 import { CheckForChanges } from "meos-api-helper";
 
-export function checkForChanges(): Promise<boolean> {
+export async function checkForChanges(): Promise<boolean> {
   return new Promise<boolean>((resolve, reject) => {
     CheckForChanges(store.state.nextDifference)
       .then(data => {
-        console.log("beep");
         if (data.updates) {
           store.commit("updateNextDifference", data.key);
-          console.log("boop");
           data.runners.forEach(runner => {
             if (runner.category === store.state.pickedClass.id) {
               resolve(true);
